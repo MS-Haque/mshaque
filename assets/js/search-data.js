1f30760968ff7ee851d694890b9a1e37374a8912
MS-Haque <toshafin@gmail.com>
2025-12-10 17:59:58 +0000

    Deploying to gh-pages from @ MS-Haque/mshaque@a824ed871e6713b6b06609f3656bde86ebcf84c8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -45,7 +45,7 @@ ninja.data = [{
             window.location.href = "/mshaque/people/";
           },
         },{id: "nav-teaching",
-          title: "teaching",
+          title: "Teaching",
           description: "",
           section: "Navigation",
           handler: () => {
