c25e923b17a558020863f1c595a51ed0b5cad7e5
MS-Haque <toshafin@gmail.com>
2025-12-16 18:29:30 +0000

    Deploying to gh-pages from @ MS-Haque/mshaque@3b0361ac4250aa1520dc03466ad403b70dfbab92 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,8 +451,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-onsite-water-sampling-and-quality-measurement-device",
+          title: 'Onsite Water Sampling and Quality Measurement Device',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/mshaque/projects/1_project/";
