88d17a3ab6098bcb97ad4b91d45cd9be6fdfd080
MS-Haque <toshafin@gmail.com>
2025-12-19 17:28:24 +0000

    Deploying to gh-pages from @ MS-Haque/mshaque@b3dc2d280f825eacb74a15ff37f51e843170867c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -456,9 +456,9 @@ ninja.data = [{
           description: "UG Faculty Mentored Grant SP 2025",
           section: "Projects",handler: () => {
               window.location.href = "/mshaque/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-active-aero-rear-wing",
+          title: 'Active Aero Rear Wing',
+          description: "UG Faculty Mentored Grant Fall 2023",
           section: "Projects",handler: () => {
               window.location.href = "/mshaque/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
